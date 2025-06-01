f86a03c9368e61e45a5504b119872955f684500d
PROTTOY SAHA <121964459+MaCroDmT@users.noreply.github.com>
2025-06-01 12:47:39 +0600

    Add files via upload

---
+++ Exception/MyException.java
@@ -0,0 +1,17 @@
+class MyException extends Exception 
+
+{
+
+ private int detail;
+
+MyException (int a)
+ {
+ detail = a;
+}
+
+
+public String toString() 
+{ 
+return "MyException[" + detail + "]";
+}
+}
