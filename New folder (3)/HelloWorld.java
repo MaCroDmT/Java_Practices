010caa78e5aa6d7dded195e3ce2b7f31118800e7
PROTTOY SAHA <121964459+MaCroDmT@users.noreply.github.com>
2025-06-09 01:12:12 +0600

    Add files via upload

---
+++ New folder (3)/HelloWorld.java
@@ -0,0 +1,13 @@
+package demo.greeting;
+
+import java.util.Date;
+import java.io.*;
+
+public class HelloWorld (
+
+public static void main(String[] args) {
+
+System.out.println ("Hello World");
+
+}
+}
---
+++ New folder (4)/Whileloop.java
@@ -0,0 +1,15 @@
+class whileLoopDemo {
+    public static void main(String args[])
+    {
+        // initialization expression
+        int i = 1;
+ 
+        // test expression
+        while (i < 100000000) {
+            System.out.println("THE SUPREMACY OF PROTTOY WILL NEVER GONNA ENDED");
+ 
+            // update expression
+            i++;
+        }
+    }
+}
